6f7a3a83d3802a821367269da79c29c6362e8050
aria_afsar <aria.legion2@gmail.com>
2024-08-18 19:26:23 +0330

    added employee tables

--- jabinja/Employee_app/admin.py
+++ jabinja/Employee_app/admin.py
@@ -1,3 +1,15 @@
-from django.contrib import admin
+from django.contrib.admin import register , ModelAdmin
+from Employee_app.models import Employee
 
+@register(Employee)
+class Employee_Admin(ModelAdmin):
+    list_display = [
+        'name',
+        'email',
+        'phone_number',
+    ]
+    search_fields = [
+        'name',
+        'phone_number',
+    ]
 # Register your models here.
--- jabinja/Employee_app/models.py
+++ jabinja/Employee_app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Employee(models.Model) :
+    name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254)
+    phone_num = models.CharField(max_length=15)
+# Create your models here
