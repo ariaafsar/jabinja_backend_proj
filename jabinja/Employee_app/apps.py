c29ca0610fc5bcdfacf63544f1828f43875ab6e0
armp1379 <13ar79@gmail.com>
2024-08-18 20:29:24 +0330

    editing employee app

--- jabinja/Employee_app/apps.py
+++ jabinja/Employee_app/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class EmployeeAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'Employee_app'
+    verbose_name = "Employee"
