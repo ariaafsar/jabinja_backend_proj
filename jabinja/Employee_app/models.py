ad266b8e01e2b9c2422b9bac94ed93346cbdf518
aria_afsar <aria.legion2@gmail.com>
2024-08-19 17:05:53 +0330

    added str function to Employee_app models

--- jabinja/Employee_app/models.py
+++ jabinja/Employee_app/models.py
@@ -4,3 +4,6 @@ class Employee(models.Model) :
     name = models.CharField(max_length=50)
     email = models.EmailField(max_length=254)
     phone_number = models.CharField(max_length=15)
+
+    def __str__(self) -> str:
+        return self.name
